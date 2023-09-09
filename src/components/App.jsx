62a66f6cbc213fdd5cd4004a775897b5f0fbe3c6
AnnaKutsenko <anisinka23@gmail.com>
2023-09-09 18:15:32 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: 'jbgjnj',
+  }
+
+  // async function createUser() {
+  //   user.id = await nanoid()
+  // }
+  // onLeaveFeedback = select => {
+  //   this.setState(prevState => {
+  //     return {
+  //       [select]: prevState[select] + 1,
+  //     }
+  //   })
+  // };
+
+  // countTotalFeedback = () => {
+  //   return this.state.good + this.state.neutral;
+  // }
+
+  // countPositiveFeedbackPercentage = () => {
+  //   return Math.round(this.state.good * 100 / this.countTotalFeedback());
+  // }
+  inputChange = event => {
+    this.setState({ name: event.currentTarget.value })
+  }
+
+  render() {
+    // const { good, neutral, bad } = this.state;
+    // const options = Object.keys(this.state);
+
+    return (
+      <form>
+        <label>
+          <input
+            type="text"
+            name={this.state.name} 
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            onChange = {this.inputChange}
+            required
+          />
+        </label>
+      </form>
+    )
+  }
+}
